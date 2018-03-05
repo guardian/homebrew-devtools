d1706ab8df3df127dde1f8b863d4f3e8c2a366fb
Pascal Honoré <pascal@alseyn.net>
2018-03-05 14:07:15 +0000

    ssm version 0.7.0

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -1,9 +1,9 @@
 class Ssm < Formula
   desc "ssh replacement: CLI program that wraps SSM's EC2 Run Command"
   homepage "https://github.com/guardian/ssm-scala"
-  version "0.6.0"
-  url "https://github.com/guardian/ssm-scala/releases/download/v0.6.0/ssm.tar.gz"
-  sha256 "b984b60e47ea8d139e0c0363170a6647a062917170351f5942508e9aae9cfd5b"
+  version "0.7.0"
+  url "https://github.com/guardian/ssm-scala/releases/download/v0.7.0/ssm.tar.gz"
+  sha256 "1b23f990b156ef8f7aefdafcd2477a47dcb8f81f4b9b6d79bcbaad128f685a24"
 
   def install
     bin.install "ssm"
