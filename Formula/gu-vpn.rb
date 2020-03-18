63bf84abbca6b225d1dd95174765f2665dc70e15
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-03-18 13:11:18 +0000

    Update to use master version of script

--- Formula/gu-vpn.rb
+++ Formula/gu-vpn.rb
@@ -3,7 +3,7 @@ class GuVpn < Formula
     desc "Formula for guardian vpn access using openconnect and vpn-slice script"
     homepage "https://github.com/guardian/homebrew-devtools"
     version "1"
-    url "https://raw.githubusercontent.com/guardian/homebrew-devtools/pm-vpn-formula/scripts/run-vpn.sh"
+    url "https://raw.githubusercontent.com/guardian/homebrew-devtools/master/scripts/run-vpn.sh"
     sha256 "395e1e703e1dc3f074c49f83a96bec7fa8887e4b15674f258ef72e7180eef5ab"
   
     depends_on "openconnect"
