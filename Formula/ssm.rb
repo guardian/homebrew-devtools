5e927089747e521de12daa8b9527fc1cfc60797c
Michael Barton <mbarton@users.noreply.github.com>
2020-03-27 09:43:35 +0000

    Update ssm.rb

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -1,7 +1,7 @@
 class Ssm < Formula
   desc "ssh replacement: CLI program that wraps SSM's EC2 Run Command"
   homepage "https://github.com/guardian/ssm-scala"
-  version "1.6.0"
+  version "1.6.1"
   url "https://github.com/guardian/ssm-scala/releases/download/v1.6.0/ssm.tar.gz"
   sha256 "2ffb136dd67b68b2d85abe6efd386b981c70c4fca1ec69efb92b21d5c11c6e76"
 
