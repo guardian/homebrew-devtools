efc5026c8a2599fda72340abc6113cf8f696492a
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-09-23 09:21:57 +0100

    Update url and sha.

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -2,8 +2,8 @@ class Ssm < Formula
   desc "ssh replacement: CLI program that wraps SSM's EC2 Run Command"
   homepage "https://github.com/guardian/ssm-scala"
   version "2.0.0"
-  url "https://github.com/guardian/ssm-scala/releases/download/v1.7.0/ssm.tar.gz"
-  sha256 "4a27d806b52ad70ab746e3ba7be7c2f74196f7f498ac6c36a10ddc58f28048d3"
+  url "https://github.com/guardian/ssm-scala/releases/download/v2.0.0/ssm.tar.gz"
+  sha256 "e3de789dbf0ebe8e40911150ea78f08492d98e54c5211fb2adf283688a68ceea"
 
   def install
     bin.install "ssm"
