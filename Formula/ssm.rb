c8390796855084a755b3baa0b095ca3040272f59
Michael Barton <mbarton@users.noreply.github.com>
2021-03-25 18:26:36 +0000

    Fix version number for ssm scala

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -1,7 +1,7 @@
 class Ssm < Formula
   desc "ssh replacement: CLI program that wraps SSM's EC2 Run Command"
   homepage "https://github.com/guardian/ssm-scala"
-  version "2.0.0"
+  version "2.1.0"
   url "https://github.com/guardian/ssm-scala/releases/download/v2.1.0/ssm.tar.gz"
   sha256 "270c992db78745c662ce61d2b9eb40d7dfed8da14bdcd68a4b56b7db910a0c6c"
 
