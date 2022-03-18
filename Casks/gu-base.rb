1f91994b2a1de73c44d890a97c5d3a9a57d74cc9
Akash Askoolum <akash1810@gmail.com>
2022-03-18 14:15:20 +0000

    Simplify `gu-base` to be a collection of core tools
    We don't strictly need to install a lot of the casks listed in `gu-base`, for example VLC.
    
    Additionally, we have not standardised on any particular version managers.
    
    In this change, we strip down `gu-base` to the core tools we use
    and push the responsibility of installing applications to the user.
    
    This will reduce friction between tools one is familiar with and the tools `gu-base` installs.

--- Casks/gu-base.rb
+++ Casks/gu-base.rb
@@ -1,5 +1,5 @@
-cask 'gu-base' do 
-  version '0.0.5'
+cask 'gu-base' do
+  version '0.0.6'
   sha256 :no_check
 
   name 'Guardian Base'
@@ -15,37 +15,23 @@ cask 'gu-base' do
   depends_on formula:  'bash-completion'
   depends_on formula:  'coreutils'
   depends_on formula:  'git'
-  depends_on formula:  'hub'
   depends_on formula:  'gnupg'
   depends_on formula:  'htop'
-  depends_on formula:  'mtr'
   depends_on formula:  'tree'
   depends_on formula:  'watch'
   depends_on formula:  'awscli'
   depends_on formula:  'nginx'
 
   # dev langs
-  depends_on cask:     'adoptopenjdk/openjdk/adoptopenjdk8'
   depends_on cask:     'gu-scala'
-  depends_on formula:  'node'
-  depends_on formula:  'yarn'
 
   # guardian stuff
   depends_on formula:  'guardian/devtools/ssm'
   depends_on formula:  'guardian/devtools/dev-nginx'
 
-  # gui apps
-  depends_on cask: 'keepingyouawake'
-  depends_on cask: 'iterm2'
-  depends_on cask: 'homebrew/cask-versions/firefox-developer-edition'
-  depends_on cask: 'visualvm'
-  depends_on cask: 'intellij-idea'
-  depends_on cask: 'visual-studio-code'
   depends_on cask: 'gpg-suite'
-  depends_on cask: 'postman'
-  depends_on cask: 'vlc'
-  depends_on cask: 'rectangle'
-  depends_on cask: 'licecap'
+
+  # This is a dependency of `guardian/devtools/ssm`
   depends_on cask: 'session-manager-plugin'
 
 end
