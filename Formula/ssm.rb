807ec2d1248623135caa530832ed61c5432862ae
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-03-31 17:25:18 +0100

    Update ssm.rb to new version

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -1,9 +1,9 @@
 class Ssm < Formula
   desc "ssh replacement: CLI program that wraps SSM's EC2 Run Command"
   homepage "https://github.com/guardian/ssm-scala"
-  version "2.3.0"
-  url "https://github.com/guardian/ssm-scala/releases/download/v2.3.0/ssm.tar.gz"
-  sha256 "dfef888b9a1d21da4137cee1b73431ac538379856bcf8663a3c02cad3df1ea1d"
+  version "2.4.0"
+  url "https://github.com/guardian/ssm-scala/releases/download/v2.4.0/ssm.tar.gz"
+  sha256 "66e1d75d7eafbcab4eb68909c7415c7aa7bc475d2e6aed9c1122744d56fed468"
 
   def install
     bin.install "ssm"
