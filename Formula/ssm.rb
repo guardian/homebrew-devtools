3d8d6885ce9db7a2b5c1a11ef1d58b42921ca77d
Philip McMahon <philip.mcmahon@guardian.co.uk>
2024-01-08 13:01:46 +0000

    Release version 3.4.0 of ssm-scala

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -1,7 +1,7 @@
 class Ssm < Formula
   desc "ssh replacement: CLI program that wraps SSM's EC2 Run Command"
   homepage "https://github.com/guardian/ssm-scala"
-  version "2.4.0"
+  version "3.4.0"
   url "https://github.com/guardian/ssm-scala/releases/download/v2.4.0/ssm.tar.gz"
   sha256 "d16deabe57596adbc242fe953b24d2962aa81178ee9ab9ddeb701b6581c6da22"
 
