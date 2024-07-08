eb89b9d934e7bbc04f9a81a9c5ebcee1248a7778
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-07-08 12:10:21 +0100

    Update bucket-blocker.rb to version 2

--- Formula/bucket-blocker.rb
+++ Formula/bucket-blocker.rb
@@ -1,14 +1,14 @@
 class BucketBlocker < Formula
   desc "CLI tool for blocking public access to S3 buckets"
   homepage "https://github.com/guardian/bucket-blocker"
-  version "1.1.0"
+  version "2.0.0"
 
   if Hardware::CPU.intel?
-    url "https://github.com/guardian/bucket-blocker/releases/download/v1.1.0/bucket-blocker_darwin-amd64.tar.gz"
-    sha256 "94c6ead682bafd7f65c7bde248aa91571514f7136d117e751a1cae9165fe2fe3"
+    url "https://github.com/guardian/bucket-blocker/releases/download/v2.0.0/bucket-blocker_darwin-amd64.tar.gz"
+    sha256 "619d5e51d5fe31c770e68631d402572e7ae4cd425dc7626736ec02814572d8a3"
   else
-    url "https://github.com/guardian/bucket-blocker/releases/download/v1.1.0/bucket-blocker_darwin-arm64.tar.gz"
-    sha256 "940770811f706e17e7a3afb894794671372bf7a66341a28dabb59c09af7cd8c9"
+    url "https://github.com/guardian/bucket-blocker/releases/download/v2.0.0/bucket-blocker_darwin-arm64.tar.gz"
+    sha256 "d2be1f5b959cfeb3a2bab7679f5de98bf5b3c01fac5412a130f94bf355df4dd9"
   end
 
   def install
