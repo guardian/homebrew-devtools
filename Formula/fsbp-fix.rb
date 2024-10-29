45aa8001ad0982d3b7578dfae4ed72fb0ba96a80
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-10-29 14:58:37 +0000

    release fsbp-fix v3.0.3

--- Formula/fsbp-fix.rb
+++ Formula/fsbp-fix.rb
@@ -1,14 +1,14 @@
 class FsbpFix < Formula
     desc "CLI tool for remediating auto-fixable AWS FSBP issues"
     homepage "https://github.com/guardian/fsbp-tools"
-    version "3.0.1"
+    version "3.0.3"
   
     if Hardware::CPU.intel?
-      url "https://github.com/guardian/fsbp-tools/releases/download/v3.0.1/fsbp-fix_darwin-amd64.tar.gz"
-      sha256 "b64b3a881bb88920eff35aa1233410f79073392b72c26925e5b0eb91f307d36e"
+      url "https://github.com/guardian/fsbp-tools/releases/download/v3.0.3/fsbp-fix_darwin-amd64.tar.gz"
+      sha256 "a07358a363b603f446e8006c371e171592f8beb70a47b7a9999d626095d7d6d5"
     else
-      url "https://github.com/guardian/fsbp-tools/releases/download/v3.0.1/fsbp-fix_darwin-arm64.tar.gz"
-      sha256 "635fa239983f3ed4cbab606b4c55df371b75ea6e43fd2437135b4516423e7c8c"
+      url "https://github.com/guardian/fsbp-tools/releases/download/v3.0.3/fsbp-fix_darwin-arm64.tar.gz"
+      sha256 "1f50578dbef39f33cf60219c006a3abdf00d66b1d107d96194366faf5f5a5b74"
     end
   
     def install
