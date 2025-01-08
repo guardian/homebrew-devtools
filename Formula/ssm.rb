109d6f8fd41229ace39bc1c67cbaeff4525b678f
adamnfish <adam.fisher@guardian.co.uk>
2025-01-08 10:22:18 +0000

    Bump ssm-scala to v3.7.0

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -1,9 +1,9 @@
 class Ssm < Formula
   desc "ssh replacement: CLI program that wraps SSM's EC2 Run Command"
   homepage "https://github.com/guardian/ssm-scala"
-  version "3.6.0"
-  url "https://github.com/guardian/ssm-scala/releases/download/v3.6.0/ssm.tar.gz"
-  sha256 "4f7a98fc8af3fed5c0e389328e1e2ba6f5ac6d5e420d72d32f761285837bb502"
+  version "3.7.0"
+  url "https://github.com/guardian/ssm-scala/releases/download/v3.7.0/ssm.tar.gz"
+  sha256 "eb72dde1cca41dff32a243072e782f769795a1d2c84f5d31b36dfdd52f7179d3"
 
   def install
     bin.install "ssm"
