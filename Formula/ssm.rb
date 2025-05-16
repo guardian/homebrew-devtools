2d2298648db9699a6d04abc26fc4121a01b1dad3
kelvin-chappell <1722550+kelvin-chappell@users.noreply.github.com>
2025-05-16 10:00:46 +0100

    Bump ssm-scala to v3.7.1 (#113)

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -1,9 +1,9 @@
 class Ssm < Formula
   desc "ssh replacement: CLI program that wraps SSM's EC2 Run Command"
   homepage "https://github.com/guardian/ssm-scala"
-  version "3.7.0"
-  url "https://github.com/guardian/ssm-scala/releases/download/v3.7.0/ssm.tar.gz"
-  sha256 "eb72dde1cca41dff32a243072e782f769795a1d2c84f5d31b36dfdd52f7179d3"
+  version "3.7.1"
+  url "https://github.com/guardian/ssm-scala/releases/download/v3.7.1/ssm.tar.gz"
+  sha256 "84682ff0a05dcdc272e5d9fc2a7f26e79fe2505190d1d05cd50f2d4594e19c0b"
 
   def install
     bin.install "ssm"
