2e8fc2aad9c01d3e4cffdc5673b105163bf0d133
Natasha <67543397+NovemberTang@users.noreply.github.com>
2025-06-16 12:20:42 +0100

    Update fsbp-fix to v 3.3.0
    Allows regions to be run in parallel for ec2.2

--- Formula/fsbp-fix.rb
+++ Formula/fsbp-fix.rb
@@ -1,14 +1,14 @@
 class FsbpFix < Formula
     desc "CLI tool for remediating auto-fixable AWS FSBP issues"
     homepage "https://github.com/guardian/fsbp-tools"
-    version "3.1.2"
+    version "3.3.0"
   
     if Hardware::CPU.intel?
-      url "https://github.com/guardian/fsbp-tools/releases/download/v3.1.2/fsbp-fix_darwin-amd64.tar.gz"
-      sha256 "63d386fd727adefb606bfaadde75b36ae38807e0fc50819d3111d55af630a4f1"
+      url "https://github.com/guardian/fsbp-tools/releases/download/v3.3.0/fsbp-fix_darwin-amd64.tar.gz"
+      sha256 "b6680c3f4f315e5701e9fb9f70a28f28d5487def54780fd56ad1b603759c13dd"
     else
-      url "https://github.com/guardian/fsbp-tools/releases/download/v3.1.2/fsbp-fix_darwin-arm64.tar.gz"
-      sha256 "50139043c0d9f911816c7e4ce769ab55fe6c30fe51615f4de6d39e7d3975b798"
+      url "https://github.com/guardian/fsbp-tools/releases/download/v3.3.0/fsbp-fix_darwin-arm64.tar.gz"
+      sha256 "48b7efca8f368c2e57a3a58a7435d323291a4a9dcb59ffe1229f80b61abaa9b9"
     end
   
     def install
