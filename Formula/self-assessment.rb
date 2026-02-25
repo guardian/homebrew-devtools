5d5619f239e8336733aec8eb3f90f0c60763dd96
Zeke Hunter-Green <zeke.huntergreen@theguardian.com>
2026-02-25 17:03:19 +0000

    self assessment v2.2.1

--- Formula/self-assessment.rb
+++ Formula/self-assessment.rb
@@ -1,14 +1,14 @@
 class SelfAssessment < Formula
   desc "CLI tool for generating a report of authored and reviewed Github pull requests, as well as an optional report of Trello cards"
   homepage "https://github.com/guardian/self-assessment"
-  version "2.2.0"
+  version "2.2.1"
 
   if Hardware::CPU.intel?
-    url "https://github.com/guardian/self-assessment/releases/download/v2.2.0/self-assessment_x86_64-apple-darwin_v2.2.0.tar.gz"
-    sha256 "8ee31fd1665165340d681b3389ba9d6bfc9477fe67f109b924c4e4ab0de67d79"
+    url "https://github.com/guardian/self-assessment/releases/download/v2.2.1/self-assessment_x86_64-apple-darwin_v2.2.1.tar.gz"
+    sha256 "16f6f49a5aaa8e2aae15f87c1185c2003dbf211065c0aedbbf67b5f850380e2a"
   else
-    url "https://github.com/guardian/self-assessment/releases/download/v2.2.0/self-assessment_aarch64-apple-darwin_v2.2.0.tar.gz"
-    sha256 "ce9d34b38b142ef8c9a9f46bf415641fba58b7bc90c2b7dff293d9e664dabef5"
+    url "https://github.com/guardian/self-assessment/releases/download/v2.2.1/self-assessment_aarch64-apple-darwin_v2.2.1.tar.gz"
+    sha256 "4f3e6daacbff331bda2080a82c17867e9a1442744053b333809915c386ee9e89"
   end
 
   def install
